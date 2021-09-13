e5b38cbd683f960995c2418ed9e403bdb4d728c0
Ankit Chaurasia <sunank200@gmail.com>
2021-09-13 15:45:12 +0545

    Subset sum problem.

---
+++ dynamicProgramming/subset_sum_problem.py
@@ -0,0 +1,44 @@
+"""
+Given a set of non-negative integers, and a value sum, determine if there is a
+subset of the given set with sum equal to given sum.
+
+Example:
+
+Input: set[] = {3, 34, 4, 12, 5, 2}, sum = 9
+Output: True
+There is a subset (4, 5) with sum 9.
+
+Input: set[] = {3, 34, 4, 12, 5, 2}, sum = 30
+Output: False
+There is no subset that add up to 30.
+"""
+
+
+def subset_sum(arr, sum, n):
+    dp = [[False for i in range(sum + 1)] for i in range(len(arr) + 1)]
+
+    for i in range(n + 1):
+        dp[i][0] = True
+
+    for j in range(1, sum + 1):
+        dp[0][j] = False
+
+    for i in range(1, n + 1):
+        for j in range(1, sum + 1):
+            if arr[i - 1] <= j:
+                dp[i][j] = dp[i - 1][j - arr[i - 1]] or dp[i - 1][j]
+            else:
+                dp[i][j] = dp[i - 1][j]
+    return dp[n][sum]
+
+
+def test():
+    t1, s1 = [3, 34, 4, 12, 5, 2], 9
+    assert subset_sum(t1, s1, len(t1)) is True, "Test case 1 failed."
+
+    t2, s2 = [3, 34, 4, 12, 5, 2], 30
+    assert subset_sum(t2, s2, len(t2)) is False, "Test case 2 failed."
+
+
+if __name__ == "__main__":
+    test()
